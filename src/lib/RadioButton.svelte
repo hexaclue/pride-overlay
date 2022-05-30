515139a2b5a8c38d304b5dfaec6bc0b48398d5c8
Qy <HIHIQY1@gmail.com>
2022-05-31 00:53:05 +0200

    Add radio row

--- src/lib/RadioButton.svelte
+++ src/lib/RadioButton.svelte
@@ -1,9 +1,31 @@
 <script lang="ts">
-    export let userSelected;
+    import type { RadioOption } from "./types/radioOption";
 
+    export let option: RadioOption<any>;
     export let group: string;
 </script>
 
-<label>
-    <input type="radio" {group} />
+<label for={option.value} class="option">
+    <input
+        id={option.value}
+        type="radio"
+        name="type"
+        value={option.value}
+        bind:group
+    />
+    <!-- <img src={option.icon} alt="" /> -->
+    <span>
+        {option.label}
+    </span>
 </label>
+
+<style>
+    .option {
+        display: grid;
+        grid-auto-columns: min-content;
+        gap: 1rem;
+    }
+    .option > * {
+        grid-row: 1;
+    }
+</style>
---
+++ src/lib/RadioRow.svelte
@@ -0,0 +1,28 @@
+<script lang="ts">
+    import RadioButton from "./RadioButton.svelte";
+    import type { RadioOption } from "./types/radioOption";
+
+    export let options: RadioOption<any>[];
+
+    export let selected: string | undefined = options
+        ? options.length
+            ? options[0].value
+            : null
+        : null;
+</script>
+
+<div class="row">
+    {#each options as option}
+        <RadioButton {option} bind:group={selected} />
+    {/each}
+</div>
+
+<style>
+    .row {
+        display: flex;
+        flex-wrap: wrap;
+        justify-content: space-between;
+        align-items: center;
+        gap: 1rem 2rem;
+    }
+</style>
