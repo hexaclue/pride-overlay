e3e5d5e057b1dfc362cbc92ee39c1feec606dd6a
Qy <HIHIQY1@gmail.com>
2022-06-01 23:17:25 +0200

    transmasc & transfem flags added

--- src/lib/constants/flagColours.ts
+++ src/lib/constants/flagColours.ts
@@ -1,38 +1,74 @@
 export const flagColours = {
-    pride: [
+    "pride": [
         "#e70000",
         "#ff8c00",
         "#ffef00",
         "#00811f",
         "#0044ff",
-        "#760089",
+        "#760089"
     ],
-    asexual: ["#000000", "#a3a3a3", "#ffffff", "#800080"],
-    bisexual: ["#d9006f", "#744d98", "#0033ab"],
-    aromantic: ["#3aa63f", "#a8d47a", "#ffffff", "#aaaaaa", "#000000"],
-    agender: [
+    "asexual": [
+        "#000000",
+        "#a3a3a3",
+        "#ffffff",
+        "#800080"
+    ],
+    "bisexual": [
+        "#d9006f",
+        "#744d98",
+        "#0033ab"
+    ],
+    "aromantic": [
+        "#3aa63f",
+        "#a8d47a",
+        "#ffffff",
+        "#aaaaaa",
+        "#000000"
+    ],
+    "agender": [
         "#000000",
         "#bababa",
         "#ffffff",
         "#b9f484",
         "#ffffff",
         "#bababa",
-        "#000000",
+        "#000000"
     ],
-    bigender: ["#ee79ac", "#fdf44c", "#ffffff", "#af6dbf", "#719fe4"],
-    abrosexual: ["#75ca91", "#b3e4c7", "#ffffff", "#e695b5", "#d9446c"],
-    mlm: [
+    "bigender": [
+        "#ee79ac",
+        "#fdf44c",
+        "#ffffff",
+        "#af6dbf",
+        "#719fe4"
+    ],
+    "abrosexual": [
+        "#75ca91",
+        "#b3e4c7",
+        "#ffffff",
+        "#e695b5",
+        "#d9446c"
+    ],
+    "mlm": [
         "#0b7c6a",
         "#42a4a5",
         "#5cc8d2",
         "#f1eeff",
         "#7bade2",
         "#1483cb",
-        "#073370",
+        "#073370"
     ],
-    pansexual: ["#ff148c", "#ffda00", "#05aeff"],
-    nonbinary: ["#fff433", "#ffffff", "#9b59d0", "#000000"],
-    poc: [
+    "pansexual": [
+        "#ff148c",
+        "#ffda00",
+        "#05aeff"
+    ],
+    "nonbinary": [
+        "#fff433",
+        "#ffffff",
+        "#9b59d0",
+        "#000000"
+    ],
+    "poc": [
         "#000000",
         "#654321",
         "#e70000",
@@ -40,11 +76,19 @@ export const flagColours = {
         "#ffef00",
         "#00811f",
         "#0044ff",
-        "#760089",
+        "#760089"
     ],
-    polyamorous: ["#0000ff", "#ff0000", "#000000"],
-    polysexual: ["#f61cb9", "#07d569", "#1c92f6"],
-    queer: [
+    "polyamorous": [
+        "#0000ff",
+        "#ff0000",
+        "#000000"
+    ],
+    "polysexual": [
+        "#f61cb9",
+        "#07d569",
+        "#1c92f6"
+    ],
+    "queer": [
         "#000000",
         "#99d9ea",
         "#00a2e8",
@@ -53,48 +97,106 @@ export const flagColours = {
         "#ffc90e",
         "#fd6666",
         "#ffaec9",
+        "#000000"
+    ],
+    "transgender": [
+        "#5bcffa",
+        "#f5abb9",
+        "#ffffff",
+        "#f5abb9",
+        "#5bcffa"
+    ],
+    "transmasculine": [
+        "#f283b4",
+        "#c2e9f2",
+        "#91e0f2",
+        "#6fd4f2",
+        "#91e0f2",
+        "#c2e9f2",
+        "#f283b4"
+    ],
+    "transfeminine": [
+        "#74deff",
+        "#ffe1ed",
+        "#ffb5d6",
+        "#fe8cbf",
+        "#ffb5d6",
+        "#ffe1ed",
+        "#74deff"
+    ],
+    "demiboy": [
+        "#7f7f7f",
+        "#c3c3c3",
+        "#99d9ea",
+        "#ffffff",
+        "#99d9ea",
+        "#c3c3c3",
+        "#7f7f7f"
+    ],
+    "demigirl": [
+        "#7f7f7f",
+        "#c3c3c3",
+        "#ffaec9",
+        "#ffffff",
+        "#ffaec9",
+        "#c3c3c3",
+        "#7f7f7f"
+    ],
+    "demigender": [
+        "#7f7f7f",
+        "#c3c3c3",
+        "#faff74",
+        "#ffffff",
+        "#faff74",
+        "#c3c3c3",
+        "#7f7f7f"
+    ],
+    "genderfluid": [
+        "#f376a0",
+        "#ffffff",
+        "#ad4cbf",
         "#000000",
+        "#3f47b6"
     ],
-    transgender: ["#5bcffa", "#f5abb9", "#ffffff", "#f5abb9", "#5bcffa"],
-    demiboy: [
-        "#7f7f7f",
-        "#c3c3c3",
-        "#99d9ea",
+    "genderqueer": [
+        "#b67fdd",
         "#ffffff",
-        "#99d9ea",
-        "#c3c3c3",
-        "#7f7f7f",
+        "#49821e"
     ],
-    demigirl: [
-        "#7f7f7f",
-        "#c3c3c3",
-        "#ffaec9",
-        "#ffffff",
-        "#ffaec9",
-        "#c3c3c3",
-        "#7f7f7f",
-    ],
-    demigender: [
-        "#7f7f7f",
-        "#c3c3c3",
-        "#faff74",
-        "#ffffff",
-        "#faff74",
-        "#c3c3c3",
-        "#7f7f7f",
-    ],
-    genderfluid: ["#f376a0", "#ffffff", "#ad4cbf", "#000000", "#3f47b6"],
-    genderqueer: ["#b67fdd", "#ffffff", "#49821e"],
-    genderflux: [
+    "genderflux": [
         "#f47694",
         "#f2a2b9",
         "#cecece",
         "#7ce0f7",
         "#3ecdf9",
-        "#fff48d",
+        "#fff48d"
     ],
-    lesbian: ["#d62900", "#ff9b55", "#ffffff", "#d462a5", "#a50062"],
-    grayromantic: ["#000000", "#b2b2b2", "#ffffff", "#2da038", "#063609"],
-    graysexual: ["#740194", "#aeb1aa", "#ffffff", "#aeb1aa", "#740194"],
-    omnisexual: ["#ff9ccd", "#ff53bd", "#270046", "#675ffe", "#8ca7ff"],
+    "lesbian": [
+        "#d62900",
+        "#ff9b55",
+        "#ffffff",
+        "#d462a5",
+        "#a50062"
+    ],
+    "grayromantic": [
+        "#000000",
+        "#b2b2b2",
+        "#ffffff",
+        "#2da038",
+        "#063609"
+    ],
+    "graysexual": [
+        "#740194",
+        "#aeb1aa",
+        "#ffffff",
+        "#aeb1aa",
+        "#740194"
+    ],
+    "omnisexual": [
+        "#ff9ccd",
+        "#ff53bd",
+        "#270046",
+        "#675ffe",
+        "#8ca7ff"
+    ]
 };
